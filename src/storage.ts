421c47e98a43c57aef920eaf1f249c4592f69246
Gabriel Silva <devzgabriel@gmail.com>
2025-07-23 10:46:50 -0300

    build: 1.0.3 - use biome and fix reset method

--- src/storage.ts
+++ src/storage.ts
@@ -1,147 +1,171 @@
 type LocalStorageKey = string;
 
 export class LupiStorage {
-  private encryptKey?: string | undefined;
-  private cachedKey?: CryptoKey;
+	private encryptKey?: string | undefined;
+	private cachedKey?: CryptoKey;
 
-  constructor(encryptKey?: string) {
-    this.encryptKey = encryptKey;
-  }
-  // private encryptSync(data: string): string {
-  //   const iv = crypto.getRandomValues(new Uint8Array(12));
-  //   const encoded = new TextEncoder().encode(data);
+	constructor(encryptKey?: string) {
+		this.encryptKey = encryptKey;
+	}
+	// private encryptSync(data: string): string {
+	//   const iv = crypto.getRandomValues(new Uint8Array(12));
+	//   const encoded = new TextEncoder().encode(data);
 
-  //   // Encrypt synchronously using Base64 (not real AES encryption)
-  //   const fakeEncrypted = btoa(String.fromCharCode(...iv) + String.fromCharCode(...encoded));
-  //   return fakeEncrypted;
-  // }
+	//   // Encrypt synchronously using Base64 (not real AES encryption)
+	//   const fakeEncrypted = btoa(String.fromCharCode(...iv) + String.fromCharCode(...encoded));
+	//   return fakeEncrypted;
+	// }
 
-  // private decryptSync(encryptedData: string): string {
-  //   const decoded = atob(encryptedData);
-  //   return decoded.slice(12); // Remove IV part (first 12 bytes)
-  // }
+	// private decryptSync(encryptedData: string): string {
+	//   const decoded = atob(encryptedData);
+	//   return decoded.slice(12); // Remove IV part (first 12 bytes)
+	// }
 
-  private async getKey() {
-    if (this.cachedKey) return this.cachedKey;
-    const enc = new TextEncoder();
-    const keyMaterial = await crypto.subtle.importKey(
-      'raw',
-      enc.encode(this.encryptKey),
-      { name: 'PBKDF2' },
-      false,
-      ['deriveKey'],
-    );
-    this.cachedKey = await crypto.subtle.deriveKey(
-      {
-        name: 'PBKDF2',
-        salt: enc.encode('salt'),
-        iterations: 100000,
-        hash: 'SHA-256',
-      },
-      keyMaterial,
-      { name: 'AES-GCM', length: 256 },
-      false,
-      ['encrypt', 'decrypt'],
-    );
-    return this.cachedKey;
-  }
+	private async getKey() {
+		if (this.cachedKey) return this.cachedKey;
+		const enc = new TextEncoder();
+		const keyMaterial = await crypto.subtle.importKey(
+			"raw",
+			enc.encode(this.encryptKey),
+			{ name: "PBKDF2" },
+			false,
+			["deriveKey"],
+		);
+		this.cachedKey = await crypto.subtle.deriveKey(
+			{
+				name: "PBKDF2",
+				salt: enc.encode("salt"),
+				iterations: 100000,
+				hash: "SHA-256",
+			},
+			keyMaterial,
+			{ name: "AES-GCM", length: 256 },
+			false,
+			["encrypt", "decrypt"],
+		);
+		return this.cachedKey;
+	}
 
-  async encrypt(value: string): Promise<string> {
-    const key = await this.getKey();
-    const iv = crypto.getRandomValues(new Uint8Array(12));
-    const encoded = new TextEncoder().encode(value);
-    const encrypted = await crypto.subtle.encrypt({ name: 'AES-GCM', iv }, key, encoded);
-    const buffer = new Uint8Array(encrypted);
-    const ivAndBuffer = new Uint8Array(iv.length + buffer.length);
-    ivAndBuffer.set(iv);
-    ivAndBuffer.set(buffer, iv.length);
-    return btoa(String.fromCharCode(...ivAndBuffer));
-  }
+	async encrypt(value: string): Promise<string> {
+		const key = await this.getKey();
+		const iv = crypto.getRandomValues(new Uint8Array(12));
+		const encoded = new TextEncoder().encode(value);
+		const encrypted = await crypto.subtle.encrypt(
+			{ name: "AES-GCM", iv },
+			key,
+			encoded,
+		);
+		const buffer = new Uint8Array(encrypted);
+		const ivAndBuffer = new Uint8Array(iv.length + buffer.length);
+		ivAndBuffer.set(iv);
+		ivAndBuffer.set(buffer, iv.length);
+		return btoa(String.fromCharCode(...ivAndBuffer));
+	}
 
-  async decrypt(value: string): Promise<string> {
-    const key = await this.getKey();
-    const ivAndBuffer = new Uint8Array(
-      atob(value)
-        .split('')
-        .map((char) => char.charCodeAt(0)),
-    );
-    const iv = ivAndBuffer.slice(0, 12);
-    const buffer = ivAndBuffer.slice(12);
-    const decrypted = await crypto.subtle.decrypt({ name: 'AES-GCM', iv }, key, buffer);
-    return new TextDecoder().decode(decrypted);
-  }
+	async decrypt(value: string): Promise<string> {
+		const key = await this.getKey();
+		const ivAndBuffer = new Uint8Array(
+			atob(value)
+				.split("")
+				.map((char) => char.charCodeAt(0)),
+		);
+		const iv = ivAndBuffer.slice(0, 12);
+		const buffer = ivAndBuffer.slice(12);
+		const decrypted = await crypto.subtle.decrypt(
+			{ name: "AES-GCM", iv },
+			key,
+			buffer,
+		);
+		return new TextDecoder().decode(decrypted);
+	}
 
-  async get<T>(key: LocalStorageKey): Promise<T | null> {
-    if (!key || typeof window === 'undefined' || typeof localStorage === 'undefined') {
-      return null;
-    }
+	async get<T>(key: LocalStorageKey): Promise<T | null> {
+		if (
+			!key ||
+			typeof window === "undefined" ||
+			typeof localStorage === "undefined"
+		) {
+			return null;
+		}
 
-    try {
-      const savedState = localStorage.getItem(key);
-      if (!savedState) return null;
+		try {
+			const savedState = localStorage.getItem(key);
+			if (!savedState) return null;
 
-      console.debug(`Loaded state from localStorage: ${key}`, savedState);
+			console.debug(`Loaded state from localStorage: ${key}`, savedState);
 
-      const decrypted = this.encryptKey ? await this.decrypt(savedState) : savedState;
+			const decrypted = this.encryptKey
+				? await this.decrypt(savedState)
+				: savedState;
 
-      let parsedValue: unknown;
-      const { type, value } = JSON.parse(decrypted);
-      switch (type) {
-        case 'number':
-          parsedValue = Number(value);
-          break;
-        case 'boolean':
-          parsedValue = value === 'true';
-          break;
-        case 'object':
-        case 'array':
-          parsedValue = JSON.parse(value);
-          break;
-        default:
-          parsedValue = value;
-      }
+			let parsedValue: unknown;
+			const { type, value } = JSON.parse(decrypted);
+			switch (type) {
+				case "number":
+					parsedValue = Number(value);
+					break;
+				case "boolean":
+					parsedValue = value === "true";
+					break;
+				case "object":
+				case "array":
+					parsedValue = JSON.parse(value);
+					break;
+				default:
+					parsedValue = value;
+			}
 
-      return parsedValue as T;
-    } catch (error) {
-      console.error(`Failed to get state from localStorage: ${key}`, error);
-      return null;
-    }
-  }
+			return parsedValue as T;
+		} catch (error) {
+			console.error(`Failed to get state from localStorage: ${key}`, error);
+			return null;
+		}
+	}
 
-  async set<T>(key: LocalStorageKey, value: T): Promise<void> {
-    if (!key || typeof window === 'undefined' || typeof localStorage === 'undefined') {
-      return;
-    }
+	async set<T>(key: LocalStorageKey, value: T): Promise<void> {
+		if (
+			!key ||
+			typeof window === "undefined" ||
+			typeof localStorage === "undefined"
+		) {
+			return;
+		}
 
-    try {
-      let stringifiedValue: string;
-      let type: string;
+		try {
+			let stringifiedValue: string;
+			let type: string;
 
-      if (typeof value === 'object' || Array.isArray(value)) {
-        stringifiedValue = JSON.stringify(value);
-        type = Array.isArray(value) ? 'array' : 'object';
-      } else {
-        stringifiedValue = String(value);
-        type = typeof value;
-      }
+			if (typeof value === "object" || Array.isArray(value)) {
+				stringifiedValue = JSON.stringify(value);
+				type = Array.isArray(value) ? "array" : "object";
+			} else {
+				stringifiedValue = String(value);
+				type = typeof value;
+			}
 
-      const dataToStore = JSON.stringify({ type, value: stringifiedValue });
-      const encrypted = this.encryptKey ? await this.encrypt(dataToStore) : dataToStore;
-      localStorage.setItem(key, encrypted);
-    } catch (error) {
-      console.error(`Failed to set state to localStorage: ${key}`, error);
-    }
-  }
+			const dataToStore = JSON.stringify({ type, value: stringifiedValue });
+			const encrypted = this.encryptKey
+				? await this.encrypt(dataToStore)
+				: dataToStore;
+			localStorage.setItem(key, encrypted);
+		} catch (error) {
+			console.error(`Failed to set state to localStorage: ${key}`, error);
+		}
+	}
 
-  del(key: LocalStorageKey): void {
-    if (!key || typeof window === 'undefined' || typeof localStorage === 'undefined') {
-      return;
-    }
+	del(key: LocalStorageKey): void {
+		if (
+			!key ||
+			typeof window === "undefined" ||
+			typeof localStorage === "undefined"
+		) {
+			return;
+		}
 
-    try {
-      localStorage.removeItem(key);
-    } catch (error) {
-      console.error(`Failed to delete state from localStorage: ${key}`, error);
-    }
-  }
+		try {
+			localStorage.removeItem(key);
+		} catch (error) {
+			console.error(`Failed to delete state from localStorage: ${key}`, error);
+		}
+	}
 }
--- src/store.ts
+++ src/store.ts
@@ -13,113 +13,129 @@ type CopyWith<T> = (updater: Updater<T> | Partial<T>) => void;
 type ValidatorFn<T> = (state: T) => string[];
 
 // Types: Actions
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// biome-ignore lint/suspicious/noExplicitAny: this is a placeholder
 type ExternalActionParams = any[];
-type InternalActionFn<T> = (state: T, ...args: ExternalActionParams) => Promise<T> | T;
+type InternalActionFn<T> = (
+	state: T,
+	...args: ExternalActionParams
+) => Promise<T> | T;
 type ActionsDeclaration<T> = Record<string, InternalActionFn<T>>;
 type ActionFn = (...args: ExternalActionParams) => void;
 
 type StoreConstructorProps<T, A extends ActionsDeclaration<T>> = {
-  storage: LupiStorage;
-  storageKey?: string | undefined;
-  actions?: A | undefined;
+	storage: LupiStorage;
+	storageKey?: string | undefined;
+	actions?: A | undefined;
 };
 
 class Store<T, A extends ActionsDeclaration<T>> {
-  private initialState: T;
-  private state: T;
-  actions?: A;
-  private listeners: Set<Listener<T>>;
-  private storageKey?: string | undefined;
-  private storage: LupiStorage;
-  private debounceTimeout: ReturnType<typeof setTimeout> | null = null;
-  private debounceDelayInMs = 300;
+	private initialState: T;
+	private state: T;
+	actions?: A;
+	private listeners: Set<Listener<T>>;
+	private storageKey?: string | undefined;
+	private storage: LupiStorage;
+	private debounceTimeout: ReturnType<typeof setTimeout> | null = null;
+	private debounceDelayInMs = 300;
 
-  constructor(initialState: T, { storage, storageKey, actions }: StoreConstructorProps<T, A>) {
-    this.storage = storage;
-    this.storageKey = storageKey;
-    this.initialState = initialState;
-    this.state = initialState;
-    this.actions = actions ?? ({} as A);
+	constructor(
+		initialState: T,
+		{ storage, storageKey, actions }: StoreConstructorProps<T, A>,
+	) {
+		this.storage = storage;
+		this.storageKey = storageKey;
+		this.initialState = initialState;
+		this.state = initialState;
+		this.actions = actions ?? ({} as A);
 
-    this._loadState(initialState).then((state) => {
-      this.state = state;
-      this.listeners.forEach((callback) => callback(this.state));
-    });
+		this._loadState(initialState).then((state) => {
+			this.state = state;
+			this.listeners.forEach((callback) => callback(this.state));
+		});
 
-    this.listeners = new Set();
-  }
+		this.listeners = new Set();
+	}
 
-  private async _loadState(initialState: T): Promise<T> {
-    if (!this.storageKey) return initialState;
-    if (!this.storage) {
-      console.error('No storage instance available');
-      return initialState;
-    }
-    const savedState = await this.storage.get<T>(this.storageKey);
-    // TODO: Add runtime validation here
+	private async _loadState(initialState: T): Promise<T> {
+		if (!this.storageKey) return initialState;
+		if (!this.storage) {
+			console.error('No storage instance available');
+			return initialState;
+		}
+		const savedState = await this.storage.get<T>(this.storageKey);
+		// TODO: Add runtime validation here
 
-    return savedState ?? initialState;
-  }
+		return savedState ?? initialState;
+	}
 
-  private async _persistState(): Promise<void> {
-    if (this.debounceTimeout) {
-      clearTimeout(this.debounceTimeout);
-    }
-    this.debounceTimeout = setTimeout(async () => {
-      if (this.storageKey) {
-        await this.storage.set(this.storageKey, this.state);
-      }
-    }, this.debounceDelayInMs);
-  }
+	private async _persistState(): Promise<void> {
+		if (this.debounceTimeout) {
+			clearTimeout(this.debounceTimeout);
+		}
+		this.debounceTimeout = setTimeout(async () => {
+			if (this.storageKey) {
+				await this.storage.set(this.storageKey, this.state);
+			}
+		}, this.debounceDelayInMs);
+	}
 
-  subscribe(callback: Listener<T>): Unsubscribe {
-    this.listeners.add(callback);
-    return () => this.listeners.delete(callback);
-  }
-  getState(): T {
-    return this.state;
-  }
+	subscribe(callback: Listener<T>): Unsubscribe {
+		this.listeners.add(callback);
+		return () => this.listeners.delete(callback);
+	}
+	getState(): T {
+		return this.state;
+	}
 
-  async update(updater: Updater<T>): Promise<void> {
-    this.state = updater(this.state);
-    await this._persistState();
-    this.listeners.forEach((callback) => callback(this.state));
-  }
+	async update(updater: Updater<T>): Promise<void> {
+		this.state = updater(this.state);
+		await this._persistState();
+		this.listeners.forEach((callback) => callback(this.state));
+	}
 
-  async set(newState: Partial<T>): Promise<void> {
-    this.state = { ...this.state, ...newState };
-    await this._persistState();
-    this.listeners.forEach((callback) => callback(this.state));
-  }
+	async set(newState: Partial<T>): Promise<void> {
+		if (
+			newState === null ||
+			newState === undefined ||
+			typeof newState !== 'object'
+		) {
+			this.state = this.initialState;
+		} else {
+			this.state = { ...this.state, ...newState };
+		}
+		await this._persistState();
+		this.listeners.forEach((callback) => callback(this.state));
+	}
 
-  async reset(): Promise<void> {
-    this.state = this.initialState;
-    await this._persistState();
-    this.listeners.forEach((callback) => callback(this.state));
-  }
+	async reset(): Promise<void> {
+		this.state = this.initialState;
+		await this._persistState();
+		this.listeners.forEach((callback) => callback(this.state));
+	}
 
-  async dispatch<K extends keyof A>(
-    actionName: K,
-    ...args: Parameters<A[K]> extends [T, ...infer P] ? P : never
-  ): Promise<void> {
-    if (this.actions && this.actions[actionName]) {
-      this.state = await this.actions[actionName](this.state, ...args);
-      await this._persistState();
-      this.listeners.forEach((callback) => callback(this.state));
-    } else {
-      console.debug(`Action "${String(actionName)}" not found`);
-    }
-  }
+	async dispatch<K extends keyof A>(
+		actionName: K,
+		...args: Parameters<A[K]> extends [T, ...infer P] ? P : never
+	): Promise<void> {
+		if (this.actions?.[actionName]) {
+			this.state = await this.actions[actionName](this.state, ...args);
+			await this._persistState();
+			this.listeners.forEach((callback) => callback(this.state));
+		} else {
+			console.debug(`Action "${String(actionName)}" not found`);
+		}
+	}
 }
 
 type StoreHookOutput<T, A extends Record<string, ActionFn>> = {
-  state: T;
-  copyWith: CopyWith<T>;
-  reset: () => void;
-  actions: {
-    [K in keyof A]: (...args: Parameters<A[K]> extends [T, ...infer P] ? P : never) => void;
-  };
+	state: T;
+	copyWith: CopyWith<T>;
+	reset: () => void;
+	actions: {
+		[K in keyof A]: (
+			...args: Parameters<A[K]> extends [T, ...infer P] ? P : never
+		) => void;
+	};
 };
 
 /**
@@ -137,51 +153,56 @@ type StoreHookOutput<T, A extends Record<string, ActionFn>> = {
  * The updater function can either be a partial state object or a function that receives the current state and returns a new state.
  */
 function useStore<T, A extends Record<string, InternalActionFn<T>>>(
-  store: Store<T, A>,
+	store: Store<T, A>,
 ): StoreHookOutput<T, A> {
-  const [value, setValue] = useState(store.getState());
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const stableSetValue = useCallback(setValue, []);
+	const [value, setValue] = useState(store.getState());
+	const stableSetValue = useCallback(setValue, []);
 
-  useEffect(() => {
-    const unsubscribe = store.subscribe(stableSetValue);
-    return unsubscribe;
-  }, [store, stableSetValue]);
+	useEffect(() => {
+		const unsubscribe = store.subscribe(stableSetValue);
+		return unsubscribe;
+	}, [store, stableSetValue]);
 
-  const actions = useMemo(() => {
-    const externalActions: {
-      [K in keyof A]: (...args: Parameters<A[K]> extends [T, ...infer P] ? P : never) => void;
-    } = {} as {
-      [K in keyof A]: (...args: Parameters<A[K]> extends [T, ...infer P] ? P : never) => void;
-    };
-    if (store.actions) {
-      for (const actionName in store.actions) {
-        externalActions[actionName] = (
-          ...args: Parameters<A[typeof actionName]> extends [T, ...infer P] ? P : never
-        ) => {
-          store.dispatch(actionName, ...args);
-        };
-      }
-    }
-    return externalActions;
-  }, [store]);
+	const actions = useMemo(() => {
+		const externalActions: {
+			[K in keyof A]: (
+				...args: Parameters<A[K]> extends [T, ...infer P] ? P : never
+			) => void;
+		} = {} as {
+			[K in keyof A]: (
+				...args: Parameters<A[K]> extends [T, ...infer P] ? P : never
+			) => void;
+		};
+		if (store.actions) {
+			for (const actionName in store.actions) {
+				externalActions[actionName] = (
+					...args: Parameters<A[typeof actionName]> extends [T, ...infer P]
+						? P
+						: never
+				) => {
+					store.dispatch(actionName, ...args);
+				};
+			}
+		}
+		return externalActions;
+	}, [store]);
 
-  return {
-    state: value,
-    copyWith: (updater) =>
-      typeof updater === 'function'
-        ? store.update(updater as Updater<T>)
-        : store.set(updater as Partial<T>),
-    reset: store.reset,
-    actions,
-  };
+	return {
+		state: value,
+		copyWith: (updater) =>
+			typeof updater === 'function'
+				? store.update(updater as Updater<T>)
+				: store.set(updater as Partial<T>),
+		reset: () => store.reset(),
+		actions,
+	};
 }
 
 type CreateStoreOptions<T, A extends ActionsDeclaration<T>> = {
-  storageKey?: string | undefined;
-  actions?: A | undefined;
-  encryptKey?: string;
-  validators?: ValidatorFn<T>[];
+	storageKey?: string | undefined;
+	actions?: A | undefined;
+	encryptKey?: string;
+	validators?: ValidatorFn<T>[];
 };
 
 /**
@@ -197,14 +218,14 @@ type CreateStoreOptions<T, A extends ActionsDeclaration<T>> = {
  * @returns {() => StoreHookOutput<T, A>} A function that returns the store hook output.
  */
 export function createStore<T, A extends ActionsDeclaration<T>>(
-  initialState: T,
-  options?: CreateStoreOptions<T, A>,
+	initialState: T,
+	options?: CreateStoreOptions<T, A>,
 ): () => StoreHookOutput<T, A> {
-  const storage = new LupiStorage(options?.encryptKey);
-  const store = new Store<T, A>(initialState, {
-    storage,
-    storageKey: options?.storageKey,
-    actions: options?.actions,
-  });
-  return () => useStore(store);
+	const storage = new LupiStorage(options?.encryptKey);
+	const store = new Store<T, A>(initialState, {
+		storage,
+		storageKey: options?.storageKey,
+		actions: options?.actions,
+	});
+	return () => useStore(store);
 }
